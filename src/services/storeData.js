9ebc90fd2afbc9a06a6da90ec04aa774d9ee5798
Roni Eka S <roni_eka.konsultan@work.bri.co.id>
2024-05-23 08:40:46 +0700

    change firestore to predictions

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,14 +3,14 @@ const { Firestore } = require('@google-cloud/firestore');
 async function storeData(id, data) {
   const db = new Firestore();
 
-  const predictCollection = db.collection('default');
+  const predictCollection = db.collection('predictions ');
   return predictCollection.doc(id).set(data);
 }
 
 async function getAllData() {
   const db = new Firestore();
   try {
-    const snapshot = await db.collection('default').get(); // Get all documents in the collection
+    const snapshot = await db.collection('predictions ').get(); // Get all documents in the collection
     const data = [];
 
     snapshot.forEach((doc) => {
